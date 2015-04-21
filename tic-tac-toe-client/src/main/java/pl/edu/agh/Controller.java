59bc5415e03b9531c05481efe3ca624acdcf6c52
Hoobie <hoobie92@gmail.com>
2015-04-21 22:09:41 +0200

    Add basic tic-tac-toe layout

---
+++ tic-tac-toe-client/src/main/java/pl/edu/agh/Controller.java
@@ -0,0 +1,9 @@
+package pl.edu.agh;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Controller {
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
+
+}
--- tic-tac-toe-client/src/main/java/pl/edu/agh/MainApp.java
+++ tic-tac-toe-client/src/main/java/pl/edu/agh/MainApp.java
@@ -1,9 +1,15 @@
 package pl.edu.agh;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +17,7 @@ import org.slf4j.LoggerFactory;
 public class MainApp extends Application {
 
     private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+    private static Button[][] buttons = new Button[3][3];
 
     public static void main(String[] args) throws Exception {
         launch(args);
@@ -18,7 +25,7 @@ public class MainApp extends Application {
 
     public void start(Stage stage) throws Exception {
 
-        log.info("Starting Hello JavaFX and Maven demonstration application");
+        log.info("Starting application.");
 
         String fxmlFile = "/fxml/hello.fxml";
         log.debug("Loading FXML for main view from: {}", fxmlFile);
@@ -29,7 +36,24 @@ public class MainApp extends Application {
         Scene scene = new Scene(rootNode);
         scene.getStylesheets().add("/styles/styles.css");
 
-        stage.setTitle("Hello JavaFX and Maven");
+        GridPane gridPane = (GridPane) scene.lookup("#gridPane");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                final Button button = new Button();
+                button.setMaxWidth(Double.MAX_VALUE);
+                button.setMaxHeight(Double.MAX_VALUE);
+                button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    public void handle(MouseEvent event) {
+                        button.setText("X");
+                        // TODO: change turn
+                    }
+                });
+                gridPane.add(button, i, j);
+                buttons[i][j] = button;
+            }
+        }
+
+        stage.setTitle("Tic-Tac-Toe");
         stage.setScene(scene);
         stage.show();
     }
